771a13a1f5bce965cd0e2c6bbe452774ca822acb
Armin Pfäffle <mail@armin-pfaeffle.de>
2023-07-19 08:57:11 +0200

    Moved assembly info to project file

--- src/XPTable/AssemblyInfo.githash.cs
+++
@@ -1,2 +0,0 @@
-using System.Reflection; 
-[assembly: AssemblyInformationalVersion("git:4920c58")] 
--- src/XPTable/createGitStatus.bat
+++ src/XPTable/createGitStatus.bat
@@ -1,7 +1,9 @@
 git log --pretty=oneline -n1 > %1
 git status >> %1
 
-git rev-parse --short HEAD > githash.txt
-set /p HASH=<githash.txt
-echo using System.Reflection; > %2
-echo [assembly: AssemblyInformationalVersion("git:%HASH%")] >> %2
+:: TODO: Write value to XPTable.csproj into InformationalVersion
+:: TODO: Why not convert the whole script into PowerShell?
+::git rev-parse --short HEAD > githash.txt
+::set /p HASH=<githash.txt
+::echo using System.Reflection; > %2
+::echo [assembly: AssemblyInformationalVersion("git:%HASH%")] >> %2
