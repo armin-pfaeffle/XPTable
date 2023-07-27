181fdb8d0344fb9a4e2c8b4e0d44e82afac8827e
Armin Pfäffle <mail@armin-pfaeffle.de>
2023-07-27 15:31:20 +0200

    Applied PR 27

--- src/XPTable/Filters/TextColumnFilter.cs
+++ src/XPTable/Filters/TextColumnFilter.cs
@@ -1,168 +1,174 @@
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
 using XPTable.Events;
 using XPTable.Models;
 
 namespace XPTable.Filters
 {
-    /// <summary>
-    /// Implements filtering for Text columns and shows a drop-down check list for a user to choose what is shown.
-    /// </summary>
-    public class TextColumnFilter : IColumnFilter
-    {
-        /// <summary>
-        /// Contains the items that were checked when the dialog was previously shown, or null if everything is checked.
-        /// </summary>
-        List<string> _allowedItems;
+	/// <summary>
+	/// Implements filtering for Text columns and shows a drop-down check list for a user to choose what is shown.
+	/// </summary>
+	public class TextColumnFilter : IColumnFilter
+	{
+		/// <summary>
+		/// Contains the items that were checked when the dialog was previously shown, or null if everything is checked.
+		/// </summary>
+		List<string> _allowedItems;
 
-        /// <summary>
-        /// Creates a new TextColumnFilter
-        /// </summary>
-        public TextColumnFilter(Column column)
-        {
-            Column = column;
-            _allowedItems = null;
-        }
+		/// <summary>
+		/// Creates a new TextColumnFilter
+		/// </summary>
+		public TextColumnFilter( Column column )
+		{
+			Column = column;
+			_allowedItems = null;
+		}
 
-        /// <summary>
-        /// The column this filter acts upon
-        /// </summary>
-        public Column Column { get; set; }
+		/// <summary>
+		/// The column this filter acts upon
+		/// </summary>
+		public Column Column { get; set; }
 
-        /// <summary>
-        /// Returns true if this filter is 'active' i.e. would actually affect the display.
-        /// </summary>
-        public bool IsFilterActive
-        {
-            get { return _allowedItems != null; }
-        }
+		/// <summary>
+		/// Returns true if this filter is 'active' i.e. would actually affect the display.
+		/// </summary>
+		public bool IsFilterActive
+		{
+			get { return _allowedItems != null; }
+		}
 
-        /// <summary>
-        /// Called to determine whether this cell can be shown by this filter
-        /// </summary>
-        /// <param name="cell"></param>
-        /// <returns></returns>
-        public bool CanShow(Cell cell)
-        {
-            if (_allowedItems == null)
-                return true;
+		/// <summary>
+		/// Called to determine whether this cell can be shown by this filter
+		/// </summary>
+		/// <param name="cell"></param>
+		/// <returns></returns>
+		public virtual bool CanShow( Cell cell )
+		{
+			if ( _allowedItems == null )
+				return true;
 
-            if (cell == null)
-                return true;
+			if ( cell == null )
+				return true;
 
-            return _allowedItems.Contains(cell.Text);
-        }
+			return _allowedItems.Contains( cell.Text );
+		}
 
-        /// <summary>
-        /// Called when the filter button is clicked on this column
-        /// </summary>
-        /// <param name="e"></param>
-        public void OnHeaderFilterClick(HeaderMouseEventArgs e)
-        {
-            TextColumnFilterDialog dialog = CreateFilterDialog(e);
+		/// <summary>
+		/// Called when the filter button is clicked on this column
+		/// </summary>
+		/// <param name="e"></param>
+		public void OnHeaderFilterClick( HeaderMouseEventArgs e )
+		{
+			var dialog = InitFilterDialog( e );
 
-            AddItems(dialog, e.Table, e.Index);
+			var filterDialog = dialog as ITextColumnFilterDialog;
 
-            DialogResult result = dialog.ShowDialog();
+			AddItems( filterDialog, e.Table, e.Index );
 
-            if (result == DialogResult.Cancel)
-                return;
+			DialogResult result = dialog.ShowDialog( );
 
-            UpdateFilter(e, dialog);
-        }
+			if ( result == DialogResult.Cancel )
+				return;
 
-        TextColumnFilterDialog CreateFilterDialog(HeaderMouseEventArgs e)
-        {
-            var dialog = new TextColumnFilterDialog();
+			UpdateFilter( e, filterDialog );
+		}
 
-            Point screenPos = e.Table.PointToScreen(new Point(e.HeaderRect.Left, e.HeaderRect.Bottom));
+		ITextColumnFilterDialog InitFilterDialog( HeaderMouseEventArgs e )
+		{
+			ITextColumnFilterDialog form = CreateFilterDialog( );
 
-            dialog.StartPosition = FormStartPosition.Manual;
-            
-            dialog.Location = screenPos;
+			Point screenPos = e.Table.PointToScreen( new Point( e.HeaderRect.Left, e.HeaderRect.Bottom ) );
 
-            return dialog;
-        }
+			form.StartPosition = FormStartPosition.Manual;
 
-        void AddItems(TextColumnFilterDialog dialog, Table table, int col)
-        {
-            string[] toAdd = GetDistinctItems(table, col);
+			form.Location = screenPos;
 
-            foreach (string item in toAdd)
-            {
-                dialog.AddItem(item, ItemIsChecked(item));
-            }
-        }
+			return form;
+		}
 
-        /// <summary>
-        /// Returns a list of distinct items from the given column
-        /// </summary>
-        /// <param name="table"></param>
-        /// <param name="col"></param>
-        /// <returns></returns>
-        public string[] GetDistinctItems(Table table, int col)
-        {
-            if (table?.TableModel == null)
-            {
-                return null;
-            }
+		/// <summary>
+		/// Create the dialog that shows the filter items. This must be a Form that also implements ITextColumnFilterDialog
+		/// </summary>
+		/// <returns></returns>
+		public virtual ITextColumnFilterDialog CreateFilterDialog( )
+		{
+			return new TextColumnFilterDialog( );
+		}
 
-            var list = new List<string>();
+		void AddItems( ITextColumnFilterDialog dialog, Table table, int col )
+		{
+			string[ ] toAdd = GetDistinctItems( table, col );
 
-            foreach (Row row in table.TableModel.Rows)
-            {
-                Cell cell = row.Cells[col];
+			foreach ( string item in toAdd )
+			{
+				dialog.AddItem( item, ItemIsChecked( item ) );
+			}
+		}
 
-                if (cell == null)
-                {
-                    continue;
-                }
+		/// <summary>
+		/// Returns a list of distinct items from the given column
+		/// </summary>
+		/// <param name="table"></param>
+		/// <param name="col"></param>
+		/// <returns></returns>
+		public string[ ] GetDistinctItems( Table table, int col )
+		{
+			if ( table?.TableModel == null )
+			{
+				return null;
+			}
 
-                string text = cell.Text;
-                if (!list.Contains(text))
-                {
-                    list.Add(text);
-                }
-            }
+			var list = new List<string>( );
 
-            return list.ToArray();
-        }
+			foreach ( Row row in table.TableModel.Rows )
+			{
+				Cell cell = row.Cells[col];
 
-        bool ItemIsChecked(string item)
-        {
-            if (_allowedItems == null)
-                return true;
+				if ( cell == null )
+				{
+					continue;
+				}
 
-            return _allowedItems.Contains(item);
-        }
+				string text = cell.Text;
+				if ( !list.Contains( text ) )
+				{
+					list.Add( text );
+				}
+			}
 
-        void UpdateFilter(HeaderMouseEventArgs e, TextColumnFilterDialog dialog)
-        {
-            if (dialog.AnyUncheckedItems())
-                SetFilterItems(dialog.GetCheckedItems());
-            else
-                SetFilterItems(null);   // The user has ticked every item - so no filtering is needed
+			return list.ToArray( );
+		}
 
-            e.Table.OnHeaderFilterChanged(e);
-        }
+		bool ItemIsChecked( string item )
+		{
+			if ( _allowedItems == null )
+				return true;
 
-        /// <summary>
-        /// Sets the filter to only display these values.
-        /// </summary>
-        /// <param name="items"></param>
-        public void SetFilterItems(IEnumerable<string> items)
-        {
-            if (items == null)
-            {
-                _allowedItems = null;
-            }
-            else
-            {
-                _allowedItems = new List<string>(items);
-            }
-        }
-    }
+			return _allowedItems.Contains( item );
+		}
+
+		void UpdateFilter( HeaderMouseEventArgs e, ITextColumnFilterDialog dialog )
+		{
+			if ( dialog.AnyUncheckedItems( ) )
+				SetFilterItems( dialog.GetCheckedItems( ) );
+			else
+				SetFilterItems( null ); // The user has ticked every item - so no filtering is needed
+
+			e.Table.OnHeaderFilterChanged( e );
+		}
+
+		/// <summary>
+		/// Sets the filter to only display these values.
+		/// </summary>
+		/// <param name="items"></param>
+		public void SetFilterItems( IEnumerable<string> items )
+		{
+			if ( items == null )
+			{
+				_allowedItems = null;
+			}
+			else
+			{
+				_allowedItems = new List<string>( items );
+			}
+		}
+	}
 }
